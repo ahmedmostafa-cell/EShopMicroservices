80a9d2c2f498328b791a536e25f2d6e799987fcf
ahmedmostafagamein <ahmed.mostafa@gamein.ae>
2025-07-08 00:01:43 +0300

    primary implementation of basket api

---
+++ src/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCartItem> Items { get; set; } = new();
+        public decimal TotalPrice => Items.Sum(a => a.Quantity * a.Price);
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public ShoppingCart()
+        {
+            
+        }
+    }
+}
