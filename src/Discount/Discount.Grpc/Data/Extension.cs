61c50048cf50213c7bef2e9acae604bce7e2f4b4
ahmedmostafagamein <ahmed.mostafa@gamein.ae>
2025-07-13 08:55:21 +0300

    finish discount docker

--- src/Discount/Discount.Grpc/Data/Extension.cs
+++ src/Discount/Discount.Grpc/Data/Extension.cs
@@ -7,7 +7,16 @@ public static class Extension
         using (var scope = app.ApplicationServices.CreateScope())
         {
             var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
-            dbContext.Database.Migrate();
+            try 
+            {
+                dbContext.Database.Migrate();
+
+            }
+            catch(Exception ex)
+            {
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<DiscountContext>>();
+                logger.LogError(ex, "An error occurred while migrating the database.");
+            }
         }
 
         return app;
