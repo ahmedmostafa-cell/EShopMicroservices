6f753fb236e11a4c3c4e1a0ecbabda4a57446f39
ahmedmostafagamein <ahmed.mostafa@gamein.ae>
2025-10-04 15:26:13 +0300

    add to app setting the routing of ordering , catalog and basket

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));  
 
 var app = builder.Build();
 
 // Configure HTTP request for pipeline
+app.MapReverseProxy();
 
 app.Run();
