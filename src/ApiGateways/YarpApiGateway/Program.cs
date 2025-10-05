ee96584b6a77201b7b69085f8b15c54967fbf122
ahmedmostafagamein <ahmed.mostafa@gamein.ae>
2025-10-05 21:00:50 +0300

    rate limiting policy apply for yarp api

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,12 +1,24 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));  
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.PermitLimit = 5;
+        options.Window = TimeSpan.FromSeconds(10);
+    });
+});
+
 var app = builder.Build();
 
 // Configure HTTP request for pipeline
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
