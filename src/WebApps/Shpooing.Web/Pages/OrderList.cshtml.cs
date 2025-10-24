c3a6b2d093983865fa72fead6315bf0b83ab0118
ahmed <ahmed@AHMED_MOSTAFA>
2025-10-24 18:52:40 +0300

    orderlist page

---
+++ src/WebApps/Shpooing.Web/Pages/OrderList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shpooing.Web.Pages
+{
+    public class OrderListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
